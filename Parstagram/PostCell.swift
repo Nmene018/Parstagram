ba9473e03a3417b479938e98af247068f025d9f3
Natalie018 <57768980+Nmene018@users.noreply.github.com>
2020-10-22 04:10:50 -0400

    Without Http access from iOS

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Natalie Meneses on 10/22/20.
+//  Copyright © 2020 NatalieM. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
